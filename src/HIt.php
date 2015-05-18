e33eee2cff02c2b7f87c4dcf509b431ea5879856
usualdesigner <usualdesigner@gmail.com>
2015-05-18 22:54:11 +0300

    init

---
+++ src/HIt.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * @link https://github.com/usualdesigner/yii2-behaviors-hit
+ * @copyright Copyright (c) 2015 Aleksey Bernackiy
+ * @license http://opensource.org/licenses/BSD-3-Clause
+ */
+
+namespace usualdesigner\behavior;
+
+use Yii;
+use yii\base\Behavior;
+use yii\db\ActiveRecord;
+
+/**
+ * HItCounterBehavior
+ *
+ * @property ActiveRecord $owner
+ *
+ * @author Aleksey Bernackiy <usualdesigner@gmail.com>
+ */
+
+class Hit extends Behavior
+{
+
+}
